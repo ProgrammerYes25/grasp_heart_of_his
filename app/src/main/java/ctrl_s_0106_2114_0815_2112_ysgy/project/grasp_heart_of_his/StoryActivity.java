a7dfff7f785201fe68ae1ff6788aebc2b1481779
yscho010612@gmail.com <yscho010612@gmail.com>
2022-11-02 01:07:00 +0900

    스토리 페이지 작업 중

---
+++ app/src/main/java/ctrl_s_0106_2114_0815_2112_ysgy/project/grasp_heart_of_his/StoryActivity.java
@@ -0,0 +1,15 @@
+package ctrl_s_0106_2114_0815_2112_ysgy.project.grasp_heart_of_his;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+    }
+}
